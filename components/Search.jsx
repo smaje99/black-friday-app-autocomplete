a755995edf493d5f6b85e460a677e8ad4ed2b000
Sergio Majé <smajefranco@gmail.com>
2024-10-17 01:01:33 -0500

    ✨ Complete Search component with Algolia autocomplete

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,122 +1,81 @@
-import { createAutocomplete } from '@algolia/autocomplete-core';
-import Image from 'next/image';
-import { useMemo, useState, useRef } from 'react';
+import { useMemo, useRef, useState } from 'react'
+import { createAutocomplete } from '@algolia/autocomplete-core'
+import Link from 'next/link'
 
-function cn(...args) {
-  return args.filter(Boolean).join(' ');
-}
-
-function AutocompleteItem({ id, title, img, price }) {
+const AutocompleteItem = ({ id, title, img, price }) => {
   return (
     <li>
-      {title}
+      <Link href={`/detail/${id}`}>
+        <a className='hover:bg-blue-300 flex gap-4 p-4'>
+          <img src={img} alt={title} className='w-12 h-12 object-contain' />
+          <div>
+            <h3 className='text-sm font-semibold'>{title}</h3>
+            <p className='text-xs text-gray-600'>{price}</p>
+          </div>
+        </a>
+      </Link>
     </li>
-  );
-}
-
-function AutocompletePanel({
-  isOpen = false,
-  collections = [],
-  panelProps = {},
-  listProps = {},
-  panelRef,
-}) {
-  if (!isOpen) {
-    return null;
-  }
-
-  return (
-    <div
-      className="absolute bg-white rounded-lg shadow-lg"
-      ref={panelRef}
-      {...panelProps}
-    >
-      {
-        collections.map((collection, index) => {
-          const { items } = collection;
-
-          return (
-            <section key={`section-${index}`}>
-              {
-                items.length > 0 ? (
-                  <ul {...listProps}>
-                    {
-                      items.map(
-                        (item) => <AutocompleteItem key={item.id} {...item} />
-                      )
-                    }
-                  </ul>
-                ) : null
-              }
-            </section>
-          )
-        })
-      }
-    </div>
   )
 }
 
-export default function Search({ ...props }) {
+export default function Search (props) {
   const [autocompleteState, setAutocompleteState] = useState({
     collections: [],
-    isOpen: false,
-  });
+    isOpen: false
+  })
 
   const autocomplete = useMemo(() => createAutocomplete({
     placeholder: 'Busca tu oferta',
-    autoFocus: true,
     onStateChange: ({ state }) => setAutocompleteState(state),
     getSources: () => [{
       sourceId: 'offers-next-api',
       getItems: ({ query }) => {
-        if (!query) {
-          return [];
+        if (!!query) {
+          return fetch(`/api/search?q=${query}`)
+            .then(res => res.json())
         }
-
-        return fetch(`/api/search?q=${query}`)
-          .then((response) => response.json());
-      },
+      }
     }],
-    ...props,
-  }), [props]);
+    ...props
+  }), [props])
 
-  const formRef = useRef(null);
-  const inputRef = useRef(null);
-  const panelRef = useRef(null);
+  const formRef = useRef(null)
+  const inputRef = useRef(null)
+  const panelRef = useRef(null)
 
-  const formProps = autocomplete.getInputProps({
-    inputElement: inputRef.current,
-  });
-
+  const formProps = autocomplete.getFormProps({
+    inputElement: inputRef.current
+  })
   const inputProps = autocomplete.getInputProps({
-    inputElement: inputRef.current,
-  });
+    inputElement: inputRef.current
+  })
 
   return (
-    <search>
-      <form className="flex justify-center mb-20" {...formProps}>
-        <div className={cn(
-          "flex",
-          "relative",
-          "p-1",
-          "bg-gradient-to-tr from-purple-600 to-blue-300",
-          "rounded-full",
-          "w-2/6",
-        )}>
-          <input
-            className="flex-1 p-2 pl-8 rounded-full w-full"
-            {...inputProps}
-          />
-        </div>
-
-        <AutocompletePanel
-          isOpen={autocompleteState.isOpen}
-          collections={autocompleteState.collections}
-          panelProps={autocomplete.getPanelProps()}
-          listProps={autocomplete.getListProps()}
-          panelRef={panelRef}
-        />
-      </form>
-    </search>
-  );
+    <form ref={formRef} className="flex justify-center mb-20" {...formProps}>
+      <div className="flex relative p-1  bg-gradient-to-tr from-purple-600 to-blue-300 rounded-full w-2/6">
+        <input ref={inputRef} className='flex-1 p-2 pl-4 rounded-full w-full' {...inputProps} />
+      {
+        autocompleteState.isOpen && (
+          <div className="absolute mt-16 top-0 left-0 border border-gray-100 bg-white overflow-hidden rounded-lg shadow-lg z-10" ref={panelRef} {...autocomplete.getPanelProps()}>
+            {autocompleteState.collections.map((collection, index) => {
+              const { items } = collection
+              console.log({items})
+              return (
+                <section key={`section-${index}`}>
+                  {items.length > 0 && (
+                    <ul {...autocomplete.getListProps()}>
+                      {
+                        items.map(item => <AutocompleteItem key={item.id} {...item} />)
+                      }
+                    </ul>
+                  )}
+                </section>
+              )
+            })}
+          </div>
+        )
+      }
+      </div>
+    </form>
+  )
 }
