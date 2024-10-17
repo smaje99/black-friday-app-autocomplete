a5affead739c8829128c181d85c175723c1df9e6
Sergio Majé <smajefranco@gmail.com>
2024-10-16 23:44:13 -0500

    🚧 Create autocomplete with Algolia in Search component

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,11 +1,49 @@
+import { createAutocomplete } from '@algolia/autocomplete-core';
+import { useMemo, useState, useRef } from 'react';
+
 function cn(...args) {
   return args.filter(Boolean).join(' ');
 }
 
-export default function Search() {
+export default function Search({ ...props }) {
+  const [autocompleteState, setAutocompleteState] = useState({
+    collections: [],
+    isOpen: false,
+  });
+
+  const autocomplete = useMemo(() => createAutocomplete({
+    placeholder: 'Busca tu oferta',
+    autoFocus: true,
+    onStateChange: ({ state }) => setAutocompleteState(state),
+    getSources: () => [{
+      sourceId: 'offers-next-api',
+      getItems: ({ query }) => {
+        if (!query) {
+          return [];
+        }
+
+        return fetch(`/api/search?q=${query}`)
+          .then((response) => response.json());
+      },
+    }],
+    ...props,
+  }), [props]);
+
+  const formRef = useRef(null);
+  const inputRef = useRef(null);
+  const panelRef = useRef(null);
+
+  const formProps = autocomplete.getInputProps({
+    inputElement: inputRef.current,
+  });
+
+  const inputProps = autocomplete.getInputProps({
+    inputElement: inputRef.current,
+  });
+
   return (
     <search>
-      <form className="flex justify-center mb-20">
+      <form className="flex justify-center mb-20" {...formProps}>
         <div className={cn(
           "flex",
           "relative",
@@ -15,9 +53,8 @@ export default function Search() {
           "w-2/6",
         )}>
           <input
-            type="search"
             className="flex-1 p-2 pl-8 rounded-full w-full"
-            placeholder="Search"
+            {...inputProps}
           />
         </div>
       </form>
